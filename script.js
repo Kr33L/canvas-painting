19c5654ded934c27b96f6ee5ea94426d7180846d
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-01-01 22:20:08 +0000

    linted

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const canvas = document.querySelector("canvas");
 const clearButton = document.querySelector("#clear");
-
 const ctx = canvas.getContext("2d");
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+
+canvas.width = window.innerWidth;
+const width = canvas.width;
+
+canvas.height = window.innerHeight;
+const height = canvas.height;
 
 const clearCanvas = () => ctx.clearRect(0, 0, width, height);
 
 function draw(event) {
-  ctx.beginPath();
-  ctx.arc(event.clientX, event.clientY, 10, 0, Math.PI * 2, false);
-  ctx.fill();
+	ctx.beginPath();
+	ctx.arc(event.clientX, event.clientY, 10, 0, Math.PI * 2, false);
+	ctx.fill();
 }
 
 clearButton.addEventListener("click", clearCanvas);
 
-canvas.addEventListener("mousedown", (event) => {
-  canvas.addEventListener("mousemove", draw);
-  canvas.addEventListener("mouseup", () => {
-    canvas.removeEventListener("mousemove", draw);
-  });
+canvas.addEventListener("mousedown", () => {
+	canvas.addEventListener("mousemove", draw);
+	canvas.addEventListener("mouseup", () => {
+		canvas.removeEventListener("mousemove", draw);
+	});
 });
